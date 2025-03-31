e5d89d0af791833b61d049b7d26432df97ae3579
João Bornelli <joao.bornelli@edu.sc.senai.br>
2025-03-31 20:31:18 -0300

    Configure MQTT client with environment variables and improve message handling

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import paho.mqtt.client as mqtt
+import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def on_message(client,userData,msg):
-    print(f"{msg.topic}: {str(msg.payload)}")
-    pass
+    print(f"{msg.topic}: {str(msg.payload.decode())}")
+
+BROKER = os.getenv("MQTT_BROKER")
+PORT = 8883
+USER = os.getenv("MQTT_USER")
+PASSWORD = os.getenv("MQTT_PASSWORD")
 
 
 
-mqttClient = mqtt.Client(mqtt.API_VERSION2,client_id="GlobalListener")
+mqttClient = mqtt.Client(client_id="GlobalListener")
+mqttClient.tls_set()
 mqttClient.tls_insecure_set()
 
-mqttClient.username_pw_set("USER","PASSWORD")
-mqttClient.connect("host",8883,)
+mqttClient.username_pw_set(USER,PASSWORD)
+mqttClient.connect(BROKER,PORT)
 
-mqttClient.subscribe("topic")
-mqttClient.on_message = on_message()
+mqttClient.subscribe("#")
+mqttClient.on_message = on_message
 
 mqttClient.loop_forever()
 
+time.sleep(0.5)
 
