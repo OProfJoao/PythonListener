8e7fc7eecf478ebc6dbe1295133954e79ec63624
João Bornelli <joao.bornelli@edu.sc.senai.br>
2025-03-31 20:17:59 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+def on_message(client,userData,msg):
+    print(f"{msg.topic}: {str(msg.payload)}")
+    pass
+
+
+
+mqttClient = mqtt.Client(mqtt.API_VERSION2,client_id="GlobalListener")
+
+mqttClient.username_pw_set("USER","PASSWORD")
+mqttClient.connect("host",8883,)
+
+mqttClient.subscribe("topic")
+mqttClient.on_message = on_message()
+
+mqttClient.loop_forever()
+
+
